4536b63a279caf430556b1dabec71529381e4273
Sami Amin <samimhdamin@gmail.com>
2025-01-26 05:45:03 +0300

    follow naming pattern in fonts

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -1,21 +1,34 @@
+import 'package:advanced_docdoc_flutter/core/theming/font_weight_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:advanced_docdoc_flutter/core/theming/colors.dart';
 
 class TextStyles {
-  static TextStyle font24Black700Weight = TextStyle(
-      fontSize: 24.sp, fontWeight: FontWeight.w700, color: Colors.black);
+  static TextStyle font24BlackBold = TextStyle(
+      fontSize: 24.sp, fontWeight: FontWeightHelper.bold, color: Colors.black);
 
   static TextStyle font32BlueBold = TextStyle(
       fontSize: 32.sp,
-      fontWeight: FontWeight.bold,
+      fontWeight: FontWeightHelper.bold,
       color: ColorsManager.mainBlue);
 
   static TextStyle font13GreyRegular = TextStyle(
       fontSize: 13.sp,
-      fontWeight: FontWeight.normal,
+      fontWeight: FontWeightHelper.regular,
+      color: ColorsManager.gray);
+
+  static TextStyle font14GreyRegular = TextStyle(
+      fontSize: 14.sp,
+      fontWeight: FontWeightHelper.regular,
       color: ColorsManager.gray);
 
   static TextStyle font16WhiteSemibold = TextStyle(
-      fontSize: 16.sp, fontWeight: FontWeight.w500, color: Colors.white);
+      fontSize: 16.sp,
+      fontWeight: FontWeightHelper.semiBold,
+      color: Colors.white);
+
+  static TextStyle font24BlueBold = TextStyle(
+      fontSize: 24.sp,
+      fontWeight: FontWeightHelper.bold,
+      color: ColorsManager.mainBlue);
 }
